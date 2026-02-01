492c7cb39432b4aeca49e131a070ad2ceab3aa30
silviu <183800984+silviu28@users.noreply.github.com>
2026-02-01 19:34:55 +0200

    Simulate health check failure

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true)
+    throw Error('Deployment health check failed! Should fallback to previous version.')
   res.send('ok')
 })
 
